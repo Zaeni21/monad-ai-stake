0545c6dd21bcaf72ba93f4f6e31c70d11bc1a014
Jawir <68275706+Zaeni21@users.noreply.github.com>
2025-04-26 21:23:41 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     monadTestnet: {
       url: "https://rpc.testnet.monad.xyz",
-      accounts: ["8a3d86a561532e7d621715aedbff27a8ab90b1f3c298fefcd7862886a627beca"], // <<< GANTI MANUAL DI SINI
+      accounts: ["8dfdf542b30189e53af4de78064d83c15df1fc2989489151d0b5486a4d129d9f"], // <<< GANTI MANUAL DI SINI
       chainId: 2710,
     }
   }
