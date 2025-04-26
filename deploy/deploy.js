6f5172e3a9c10cc562cce7deb070f96c2814b8ca
Jawir <68275706+Zaeni21@users.noreply.github.com>
2025-04-26 20:29:53 +0700

    Add files via upload

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,1 +1,11 @@
+async function main() {
+    const AutoStakeManager = await ethers.getContractFactory("AutoStakeManager");
+    const autoStakeManager = await AutoStakeManager.deploy();
+    await autoStakeManager.deployed();
+    console.log("AutoStakeManager deployed to:", autoStakeManager.address);
+}
 
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
